cb8d7fe7b970a8662ace6b73f621dfdf89d7b29d
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 20:15:35 -0300

    Create Endereço.java

---
+++ Endereço.java
@@ -0,0 +1,24 @@
+package q2
+
+
+public class Endereco {
+  String rua;
+  int numero; 
+  String complemento;
+  String bairro;
+  String cidade; 
+  String uf;
+  String cep;
+  
+  public Endereco(String rua, int numero, String complemento, String bairro, String cidade, String uf, String cep){
+    this.rua = rua;
+    this.numero = numero;
+    this.complemento = complemento;
+    this.bairro = bairro;
+    this.cidade = cidade;
+    this.uf = uf;
+    this.cep = CEP;
+  }
+}
+    
+  
