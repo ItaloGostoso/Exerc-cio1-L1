ac971da2a1f0f3aab132eb35ac83faf5ddcd3643
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 20:25:19 -0300

    Create Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,20 @@
+package q2;
+
+import q2.Endereco;
+
+public class Medico {
+  int codigo;
+  String nome;
+  String sexo;
+  String Especialidades;
+  String Endereco;
+  
+  public Medico (int codigo, String nome, String sexo, String Especialidades, String Endereco){
+    this.codigo = cod;
+    this.nome = nome;
+    this.sexo = sx;
+    this.Especialidades = Esp;
+    this.Endereco = End;
+  }
+}
+    
