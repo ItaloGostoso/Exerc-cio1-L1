ae2fc7f78df001265bafaf2d76863c210b623455
ItaloGostoso <89165297+ItaloGostoso@users.noreply.github.com>
2021-10-15 21:19:25 -0300

    Create Lutador.java

---
+++ Lutador.java
@@ -0,0 +1,16 @@
+package q3;
+
+public class Lutador {
+    String nome;
+    int energia;
+    int forca;
+    public Lutador(String nome, int energia, int forca){
+        this.nome = nome;
+        this.energia = Ene;
+        this.forca = Forca;
+    }
+    
+    public void aplicarGolpe(Lutador oponente){
+        oponente.Ene -= forca;
+    }
+}
